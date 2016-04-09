0c77bcfc945834b07dcf42c3d5a39a0a7661f4cc
Mike Weaver <miketweaver@users.noreply.github.com>
2016-04-09 15:35:05 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [ -z "$STARBOUND_UNSTABLE" ]
-then
-  APP_ID="211820"
-else
-  APP_ID="367540"
-fi
-
-/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +set_steam_guard_code $STEAMGUARD_CODE +login $STEAM_USERNAME $STEAM_PASSWORD +force_install_dir /starbound/ +app_update $APP_ID +quit
-
 cd /starbound/linux64
 
 ./starbound_server
